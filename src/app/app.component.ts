12dac888578cc795265e5fc8bc99b50af5d7e389
Puneetmishra63 <puneetmishra630@gmail.com>
2023-10-06 18:39:41 +0530

    modifying the code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,22 +10,40 @@ export class AppComponent {
   sidenavWidth = '0';
   image = 'inline';
 
+   isSidenavOpen = false;
+
   toggleSidenav(): void {
-    const logo = document.querySelector('.logo') as HTMLElement;
-    this.sidenavWidth = this.sidenavWidth === '0' ? '250px' : '0';
-    if (this.sidenavWidth === '0') {
-      logo.style.display = 'inline'
+    this.isSidenavOpen = !this.isSidenavOpen;
+  }
+
+  getSideNavStyles() {
+    let styles: {
+      [key: string]: string;
+    } = {
+      'background': '#fff',
+      '-webkit-clip-path': 'polygon(0 0,45% 0,100% 100%,0% 100%)',
+      'clip-path': 'polygon(0 0,45% 0,100% 100%,0% 100%)',
+      'position': 'fixed',
+      'height': '100vh',
+      'top': '0',
+      'bottom': '0',
+      'left': '0',
+      'z-index': '99999',
+      'transition': '1s',
+      'filter': 'drop-shadow(8px 0px 4px rgba(145,144,135,0.28))'
+    };
+
+    if (this.isSidenavOpen) {
+      styles['width'] = '31vw';
+
     } else {
-      logo.style.display = 'none'
+      styles['width'] = '0';
+      // Add other closed state styles here
     }
 
+    return styles;
   }
-  hideNav(): void {
-    const logo = document.querySelector('.logo') as HTMLElement;
-    this.sidenavWidth = this.sidenavWidth === '0' ? '55px' : '0';
-    if (this.sidenavWidth === '0') {
-      logo.style.display = 'inline'
-    }
-
-  }
+ hideNav(): void {
+  this.isSidenavOpen = false; // Close the sidebar
+}
 }
